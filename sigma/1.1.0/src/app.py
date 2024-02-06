f1871dc4dfecd3bac68df1c0b24ed80230b82103
ctechmat <64774409+ctechmat@users.noreply.github.com>
2024-02-06 13:40:20 +0100

    Update app.py

--- sigma/1.1.0/src/app.py
+++ sigma/1.1.0/src/app.py
@@ -18,7 +18,7 @@ from walkoff_app_sdk.app_base import AppBase
 # 4. Make it so you can start with Mitre Att&ck techniques 
 # and automatically get the right rules set up with your tools :O
 class Sigma(AppBase):
-    __version__ = "1.0.0"
+    __version__ = "1.1.0"
     app_name = "sigma"  # this needs to match "name" in api.yaml
 
     def __init__(self, redis, logger, console_logger=None):
