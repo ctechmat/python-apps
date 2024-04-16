686077987707cc5b9ea5ade52f5fdd617eee0896
ctechmat <64774409+ctechmat@users.noreply.github.com>
2024-04-16 16:06:34 +0200

    Update app.py

--- sigma/1.1.0/src/app.py
+++ sigma/1.1.0/src/app.py
@@ -110,14 +110,14 @@ class Sigma(AppBase):
             print("FAILED to run bash: ", stdout[1])
             item = stdout[1]
     
-        try:
-            ret = item.decode("utf-8")
+        #try:
+            #ret = item.decode("utf-8")
             #self.logger.info("Ret : %s" % ret)
-            return ret
-        except Exception:
-            return item
+            #return ret
+        #except Exception:
+            #return item
     
-        return print("{rule: "+item+"}")
+        #return item
 
 if __name__ == "__main__":
     Sigma.run()
