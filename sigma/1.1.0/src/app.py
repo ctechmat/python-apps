1f5791278abdfe323e0c95c8926f5706917bced3
ctechmat <64774409+ctechmat@users.noreply.github.com>
2024-04-16 15:26:23 +0200

    Update app.py

--- sigma/1.1.0/src/app.py
+++ sigma/1.1.0/src/app.py
@@ -48,7 +48,8 @@ class Sigma(AppBase):
             with open("%s/%s" % (basedir, source.name), "wb+") as tmp:
                 filedata = source.read()
                 self.logger.info("Filedata (%s): %s" % (source.name, filedata))        
-                dict=yaml.load(filedata, Loader=SafeLoader)
+                dict=yaml.load(filedata)
+                self.logger.info("YAML: %s" % dict)
                 json_file=json.dumps(dict)
                 print(json_file)
     
